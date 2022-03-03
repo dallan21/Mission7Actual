21a41074e30c4a39c595ec55357bf84e788c41e5
dallan21 <90338790+dallan21@users.noreply.github.com>
2022-03-03 00:34:25 -0700

    Fourth

---
+++ Mission7/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+namespace Mission7.Components
+{
+    public class CartSummaryViewComponent : ViewComponent 
+    {
+        private ShoppingCart shoppingCart;
+
+        public CartSummaryViewComponent(ShoppingCart sc)
+        {
+            shoppingCart = sc;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(shoppingCart);
+        }
+    }
+}
